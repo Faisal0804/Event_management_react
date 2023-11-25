b7d230abf62399816d320293ccf4488f00906eba
Faisal Mohammedy <faisal.0408.ac@gmail.com>
2023-11-25 21:25:09 +0600

    video react

--- src/Components/TopFixedBannner/TopFixedBannner.jsx
+++ src/Components/TopFixedBannner/TopFixedBannner.jsx
@@ -1,8 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment,useState } from 'react'
 
-import { Container,Row,Col} from 'react-bootstrap'
+import { Container,Row,Col,Button, Modal} from 'react-bootstrap'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons"
+
+import Poster from "../../assets/img/about1.jpg"
+import { Player, BigPlayButton, ControlBar ,ReplayControl,ForwardControl,VolumeMenuButton} from 'video-react';
+
 
 const TopFixedBannner = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Fragment>
         <Container fluid={true} className="TopFixedBanner p-0">
@@ -13,7 +24,28 @@ const TopFixedBannner = () => {
                             <h1 className='BannerTitle'>Welcome our Website</h1>
                             <p className="BannerSubTitle">Lorem ipsum dolor sit amet.</p>
                             <a className='BannerLink' href=''>Details</a>
-                          
+                            <p className="mt-2"><FontAwesomeIcon className="playBtn" onClick={handleShow}   icon={faPlayCircle} /></p>
+
+                            <Modal size='sm'  show={show} onHide={handleClose} animation={false}>
+            
+                                <Modal.Body>
+                                <Player  
+                                     poster={Poster }               
+                                    src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+                                    <BigPlayButton position="center" />
+                                    <ControlBar autoHide={false}>                               
+                                        <ReplayControl seconds={30} order={2.3} />
+                                        <ForwardControl seconds={30} order={3.3} />                        
+                                    </ControlBar>
+                              </Player>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                <Button   variant="info" onClick={handleClose}>
+                                    Close
+                                </Button>                               
+                                </Modal.Footer>
+                            </Modal>
+                                                
                         
                         </Col>
 
@@ -22,6 +54,9 @@ const TopFixedBannner = () => {
 
 
              </div>
+
+             
+       
         </Container>
     </Fragment>
   )
